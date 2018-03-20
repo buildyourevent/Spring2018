dd1c955ad59c0da6b1cbab85c00218bf5bea251c
sijj <ijami057@gmail.com>
2018-03-20 14:57:02 -0400

    Update to design of db

--- BuildYourEvent/src/BuildYourEvent/SQL/dbcreation.sql
+++ BuildYourEvent/src/BuildYourEvent/SQL/dbcreation.sql
@@ -5,13 +5,14 @@ GO
 USE [ByeDB]
 GO
 
+
 CREATE TABLE Locations (
 	[id] smallint NOT NULL IDENTITY,
 	[city] varchar(200) NOT NULL,
 	[province] varchar(200) NOT NULL,
 	[country] varchar(200) NOT NULL,
 	[street] varchar(200) NOT NULL,
-	[postal_code] varchar(7) NOT NULL,
+	[postal_code] varchar(7) NOT NULL IDENTITY,
 	[latitude] varchar(100) NOT NULL,
 	[longitude] varchar(100) NOT NULL,
 	PRIMARY KEY ([id])
@@ -50,14 +51,12 @@ CREATE TABLE Venues (
 CREATE TABLE Amenities (
 	[id] smallint NOT NULL IDENTITY,
 	[name] varchar(200) NOT NULL,
-	[fk_amenities] smallint NOT NULL,
 	PRIMARY KEY ([id])
 );
 
 CREATE TABLE Venue_Rules (
 	[id] smallint NOT NULL IDENTITY,
 	[name] varchar(200) NOT NULL,
-	[fk_venue_rules] smallint NOT NULL,
 	PRIMARY KEY ([id])
 );
 
@@ -70,14 +69,12 @@ CREATE TABLE Styles (
 CREATE TABLE Features (
 	[id] smallint NOT NULL IDENTITY,
 	[name] varchar(200) NOT NULL,
-	[fk_features] smallint NOT NULL,
 	PRIMARY KEY ([id])
 );
 
 CREATE TABLE On_Site_Services (
 	[id] smallint NOT NULL IDENTITY,
 	[name] varchar(200) NOT NULL,
-	[fk_on_site_services] smallint NOT NULL,
 	PRIMARY KEY ([id])
 );
 
@@ -117,14 +114,6 @@ ALTER TABLE [Venues] ADD CONSTRAINT [Venues_fk5] FOREIGN KEY ([fk_feature]) REFE
 ALTER TABLE [Venues] ADD CONSTRAINT [Venues_fk6] FOREIGN KEY ([fk_on_site_service]) REFERENCES On_Site_Services([id]);
 
 ALTER TABLE [Venues] ADD CONSTRAINT [Venues_fk7] FOREIGN KEY ([fk_location]) REFERENCES Locations([id]);
-
-ALTER TABLE [Amenities] ADD CONSTRAINT [Amenities_fk0] FOREIGN KEY ([fk_amenities]) REFERENCES Amenities([id]);
-
-ALTER TABLE [Venue_Rules] ADD CONSTRAINT [Venue_Rules_fk0] FOREIGN KEY ([fk_venue_rules]) REFERENCES Venue_Rules([id]);
-
-ALTER TABLE [Features] ADD CONSTRAINT [Features_fk0] FOREIGN KEY ([fk_features]) REFERENCES Features([id]);
-
-ALTER TABLE [On_Site_Services] ADD CONSTRAINT [On_Site_Services_fk0] FOREIGN KEY ([fk_on_site_services]) REFERENCES On_Site_Services([id]);
 
 ALTER TABLE [Users] ADD CONSTRAINT [Users_fk0] FOREIGN KEY ([fk_user_type]) REFERENCES User_Types([id]);
 
