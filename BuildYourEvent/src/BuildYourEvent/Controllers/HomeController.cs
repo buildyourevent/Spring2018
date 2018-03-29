5cc173b1b86e1adc46402a1ece122daeb2429ee7
DZA <DZA@DZA-PC>
2018-03-28 21:44:53 -0400

    Changes to controller

--- BuildYourEvent/src/BuildYourEvent/Controllers/HomeController.cs
+++ BuildYourEvent/src/BuildYourEvent/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BuildYourEvent.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,13 +11,21 @@ namespace BuildYourEvent.Controllers
 {
     public class HomeController : Controller
     {
+        private VenuesDataContext _context;
+
+        public HomeController(VenuesDataContext context)
+        {
+            _context = context;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var venueTypes = (from v in _context.Venue_Types select v).ToList();
+            return View(venueTypes);
         }
         public IActionResult Results()
         {
+            //var 
 
             return View();
         }
