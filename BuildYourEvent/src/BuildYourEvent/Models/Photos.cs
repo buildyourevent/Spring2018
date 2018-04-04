f812ef6c06bac24bf0945a89e7425efe073b4772
JasonPilipchuk <35742656+JasonPilipchuk@users.noreply.github.com>
2018-04-04 16:50:02 -0400

    Added photo model

---
+++ BuildYourEvent/src/BuildYourEvent/Models/Photos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BuildYourEvent.Models
+{
+    public class Photos
+    {
+        [Required]
+        public short id
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(2048)]
+        public String filename
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(2048)]
+        public String url
+        {
+            get;
+            set;
+        }
+    }
+}
