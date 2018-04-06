b86de6b0b828b4af870d4ad78927824f19596b98
sijj <ijami057@gmail.com>
2018-04-05 23:08:53 -0400

    applied surogate key in code for Venues_types_Venues

--- BuildYourEvent/src/BuildYourEvent/Models/Venue_Types_Venues.cs
+++ BuildYourEvent/src/BuildYourEvent/Models/Venue_Types_Venues.cs
@@ -9,7 +9,7 @@ namespace BuildYourEvent.Models
 {
     public class Venue_Types_Venues
     {
-        [Key, ForeignKey("Venues")]
+        [ ForeignKey("Venues")]
         [Required]
         public short fk_Venue
         {
